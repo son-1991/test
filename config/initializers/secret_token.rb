f6d54ca5de8eac26f1aba05b12ffb8647dfc0f46
son-1991 <a-sm2219@outlook.com>
2019-11-07 12:21:40 -0800

    allfiles

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blogex::Application.config.secret_token = 'bef44babf3712b6610e7d11cc7b77fda66cfe112481ef8be2d0fae2610f23c7c09166bea52184d3fb52a4185b8701a27c5e44dceb4ac40c3ad00c07386640cab'
